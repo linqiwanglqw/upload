e5b4757e40ddbd49a6bd4587fba167d421a7b94a
爱写代码的小学生 <1305554895@qq.com>
2022-04-03 21:30:33 +0800

    修改

--- src/main/java/com/lin/service/StorageService.java
+++ src/main/java/com/lin/service/StorageService.java
@@ -8,16 +8,6 @@ import java.io.IOException;
  * 存储操作的service
  */
 public interface StorageService {
-//
-//    /**
-//     * 删除全部数据
-//     */
-//    void deleteAll();
-//
-//    /**
-//     * 初始化方法
-//     */
-//    void init();
 
     /**
      * 上传文件方法1
